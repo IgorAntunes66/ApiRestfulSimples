f79fef6b108302135f92b708b2bf27f44d862e0f
IgorAntunes66 <igorgantunes@hotmail.com>
2025-07-21 16:22:01 -0400

    Fiz a configuração do banco de dados postgreSQL e ajustei os handlers ja existentes e a main para o banco de dados

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,15 +2,27 @@ package main
 
 import (
 	"apiRestFulSimpes/internal/handler"
+	"apiRestFulSimpes/internal/pkg"
+	"apiRestFulSimpes/internal/repository"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
+
+	db, err := pkg.ConectaDB()
+	if err != nil {
+		log.Fatalf("Erro ao iniciar o banco de dados: %v", err)
+	}
+
+	repo := repository.NewRepository(db)
+	apiServer := handler.NewApiServer(*repo)
+
 	r := chi.NewRouter()
 	r.Get("/", handler.HelloHandler)
-	r.Get("/tasks", handler.TasksListHandler)
-	r.Get("/tasks/{ID}", handler.TaskHandler)
+	r.Get("/tasks", apiServer.TasksListHandler)
+	r.Get("/tasks/{ID}", apiServer.TaskHandler)
 	http.ListenAndServe(":8080", r)
 }
